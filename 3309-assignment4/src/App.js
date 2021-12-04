f2f50ec0672a396513cbc9c976d131c3fbae0c76
alexjnelson <alexj.nelson9@gmail.com>
2021-12-04 14:53:38 -0500

    Added login

--- 3309-assignment4/src/App.js
+++ 3309-assignment4/src/App.js
@@ -1,10 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 function App() {
+  const [librarian, setLibrarian] = useState(false)
+  const [email, setEmail] = useState('');
+
+  const login = () => {
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: email })
+      }
+    })
+      .then(response => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log('error: ', error);
+      })
+  }
+
+  const reserve = () => {
+
+  }
+
+  const returnBook = () => {
+
+  }
+
+  const searchBook = () => {
+
+  }
+
+  const getPopularChoice = () => {
+
+  }
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const loginForm = (
+    <Form id='login-form' className='w-50 h-50 card p-5 m-5' onSubmit={login}>
+      <Form.Group controlId='email'>
+        <Form.Label>Email: </Form.Label>
+        <Form.Control type='email' name='email' placeholder='' onChange={handleEmail} />
+      </Form.Group>
+      <Button type='submit' className='my-3'>Login</Button>
+    </Form>
+  );
+
+  const librarianView = (
+    <div>
+
+    </div>
+  )
+
   return (
-    <div className="App">
-      
+    <div className="App h-100 w-100">
+      <div id='tempbuttons'>
+        <Button onClick={reserve}>Make Reservation</Button>
+        <Button onClick={returnBook}>Return book</Button>
+        <Button onClick={searchBook}>Search Book</Button>
+        <Button onClick={getPopularChoice}>Get Popular Choice</Button>
+      </div>
+
+      {librarian ? librarianView : loginForm}
+
     </div>
   );
 }
